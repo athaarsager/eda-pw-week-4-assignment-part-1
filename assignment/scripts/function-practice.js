3a3d3a3b692372c9adeced1a09cb3bace8798c12
Andrew Haarsager <athaarsager@gmail.com>
2023-11-28 10:51:55 -0600

    Answer question 5

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -33,19 +33,21 @@ function multiplyThree(num1, num2, num3) {
   return num1 * num2 * num3;
 }
 
-console.log("Test- should return 24", multiplyThree(2, 3, 4));
+console.log("Test - should return 24", multiplyThree(2, 3, 4));
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 function isPositive(number) {
   if (number > 0) {
-    return;
+    return true;
   }
-  return;
+  return false;
 }
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
 
+console.log("Test - should return true", isPositive(5));
+console.log("Test - should return false", isPositive(-9001));
 
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
